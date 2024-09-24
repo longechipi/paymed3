61310786ebcfe490cf5bd8590496f6296e4d8096
longechipi <castilloacostajean@gmail.com>
2024-09-23 21:10:46 -0400

    Fix: updcontact

--- model/mod_seguro/updcontact.php
+++ model/mod_seguro/updcontact.php
@@ -7,8 +7,8 @@ $correo1   =$_POST['correo1'];
 $cargo1    =$_POST['cargo1'];
 $dpto1     =$_POST['dpto1'];
 
-$str="INSERT INTO asegura_contacto (idaseg, nombres, cargo, telefono, correo, dpto) 
-VALUES ('".$nocli."','".strtoupper($contacto1)."','".$cargo1."','".strtoupper($telefono)."','".strtoupper($correo1)."','".strtoupper($dpto1)."')";
+$str="INSERT INTO asegura_contacto (idaseg, idtipocontac, nombres, cargo, telefono, correo, dpto) 
+VALUES ('".$nocli."','0', '".strtoupper($contacto1)."','".$cargo1."','".strtoupper($telefono)."','".strtoupper($correo1)."','".strtoupper($dpto1)."')";
 $conexion=$mysqli->query($str);
 
 if($conexion){
